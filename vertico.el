0798eb3c98da884ef7f81a56266dffad1fd841b2
Madhu <enometh@net.meer>
2021-12-30 09:56:01 +0530

    vertico.el: hack C-k to remove entries from recentf-list
    * src/vertico.el: (vertico-kill-buffer): special case
    consult-recent-files so C-k removes entries from the
    recentf-list. This does not work as well as C-k to kill buffers
    because of limitations in the design of consult: as it prepares the
    list of candidates upfront, and cannot adapt to changes, so vertico
    ends up completing on the older list. However The next time you call
    consult-recentf-files it picks up the new recentf-list.
    
    The other problem is consult-recent-files calls abbreviate-file-name
    on the set of candidates, so what is being shown by vertico need not
    be the same entry in the recentf-list. The sub-optimal workaround is
    to delete the both expanded file name as well as the abbreviated file
    name. This may not be what the user always wants (i.e. he wants to
    delete one but not the other)
    
    TODO: rename vertico-kill-buffer to reflect the added
    responsibilities.
    
    [240317] extend to include find-file family (for file-name-history)
    and consult-buffer. unfortunately consult-dir won't work.

--- vertico.el
+++ vertico.el
@@ -710,14 +710,47 @@ The function is configured by BY, BSIZE, BINDEX, BPRED and PRED."
 
 (defun vertico-kill-buffer (&optional arg)
   (interactive "P")
-  (let (cand buf)
-    (cond ((and (eolp)
-		(setq cand (and (>= vertico--index 0) (vertico--candidate)))
-		(setq buf (get-buffer cand)))
-	   (kill-buffer buf)
-	   (setq vertico-force-exhibit t)
-	   (vertico-next))
-	  (t (kill-line arg)))))
+  (cl-labels ((deletefilehist (elt hist-var &optional (depth 0))
+		(cl-check-type hist-var symbol)
+		(let ((len (and (zerop depth)
+				(length (symbol-value hist-var)))))
+		  (set hist-var (cl-delete elt (symbol-value hist-var)
+					   :test #'equal))
+		  (if (string-match "^~/" elt)
+		      (deletefilehist (expand-file-name elt) hist-var
+				      (1+ depth)))
+		  (let ((new-len (and len (length (symbol-value hist-var)))))
+		    (when new-len
+		      (message "deleted %d elements of %s" (- len new-len)
+			       hist-var)))))
+	      (redisp ()
+		(setq vertico-force-exhibit t)
+		(vertico-next)))
+    (let (cand cmd)
+      (cond ((and (eolp)
+		  (setq cand (and t ;; (>= vertico--index 0)
+				  (vertico--candidate)))
+		  (member (setq cmd vertico--this-command)
+			  '(switch-to-buffer consult-buffer)))
+	     (when-let (buf (get-buffer (if (eq cmd 'consult-buffer)
+					    (consult--tofu-get cand)
+					  cand)))
+	       (kill-buffer buf)
+	       (redisp)))
+	    ((member cmd '(find-file
+			   find-alternate-file
+			   dired
+			   find-file-or-dired
+			   find-file-or-dired-find-alternate-file
+			   find-file-or-dired-find-file
+			   find-file-or-dired-dired))
+	     (deletefilehist cand 'file-name-history)
+	     (redisp))
+	    ((member cmd '(recentf-open recent consult-recent-file))
+	     (deletefilehist cand 'recentf-list)
+	     (redisp))
+	    (t (kill-line arg))))))
+
 
 (define-key vertico-map (kbd "C-k") 'vertico-kill-buffer)
 
